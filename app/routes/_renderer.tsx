2c59679f1de50bbb9add64819d8ca54f47cc8ea5
ryu <114303361+ryuapp@users.noreply.github.com>
2024-02-12 01:39:06 +0900

    Add tailwindcss

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -9,6 +9,11 @@ export default jsxRenderer(({ children, title }) => {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
+        {import.meta.env.PROD ? (
+          <link href='static/assets/style.css' rel='stylesheet' />
+        ) : (
+          <link href='/app/style.css' rel='stylesheet' />
+        )}
         <Script src="/app/client.ts" />
         <Style />
       </head>
