fe6929d6fbe2a690147f917acd93c88c57bc3410
ryu <114303361+ryuapp@users.noreply.github.com>
2025-02-10 01:46:59 +0900

    Use `@hono/vite-build`

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,18 @@
-import pages from '@hono/vite-cloudflare-pages'
+import build from '@hono/vite-build/cloudflare-pages'
 import honox from 'honox/vite'
-import tailwindcss from '@tailwindcss/vite';
-import client from 'honox/vite/client'
+import tailwindcss from '@tailwindcss/vite'
+import adapter from '@hono/vite-dev-server/cloudflare'
 import { defineConfig } from 'vite'
 
-export default defineConfig(({ mode }) => {
-  if (mode === 'client') {
-    return {
-      plugins: [client(), tailwindcss()],
-      build: {
-        rollupOptions: {
-          input: ["/app/style.css"]
-        },
+export default defineConfig({
+  plugins: [
+    honox({
+      client: {
+        input: ['/app/style.css'],
       },
-    }
-  }
-
-  return {
-    plugins: [honox(), pages(), tailwindcss()]
-  }
-})
+      devServer: { adapter },
+    }),
+    build(),
+    tailwindcss(),
+  ],
+});
