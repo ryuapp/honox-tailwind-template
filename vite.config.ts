a28a11d2e799f6f840ff35b7713034023b263b19
ryu <114303361+ryuapp@users.noreply.github.com>
2025-02-22 12:58:33 +0900

    Use latest template as a base

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import build from '@hono/vite-build/cloudflare-pages'
-import honox from 'honox/vite'
-import tailwindcss from '@tailwindcss/vite'
 import adapter from '@hono/vite-dev-server/cloudflare'
+import honox from 'honox/vite'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
